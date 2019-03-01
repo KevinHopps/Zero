7461cd8b0b413c2ea9cee8c487a32163d4223e6e
Kevin Hopps <khopps@adobe.com>
2019-02-28 22:59:12 -0600

    Check in xcode project.

---
+++ xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  zero
+//
+//  Created by Kevin Hopps on 2/28/19.
+//  Copyright © 2019 Kevin Hopps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
