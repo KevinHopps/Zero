0d6dab4778a43510ec120103c38f1c0b703b1398
Kevin Hopps <khopps@adobe.com>
2019-02-28 23:44:26 -0600

    Add error checking.

--- zero.cpp
+++ zero.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <sstream>
 #include "zero.h"
+
+#define THROW(stuff) do { std::ostringstream oss; oss << stuff; throw std::runtime_error(oss.str()); } while (false)
 
 std::string shift(Args& args);
 void zero(const std::string& path);
@@ -30,6 +33,8 @@ std::string shift(Args& args)
 void zero(const std::string& path)
 {
     FILE* fp = fopen(path.c_str(), "r+");
+    if (fp == nullptr)
+        THROW(path << ": cannot open");
     char buf[4096];
     char zero[sizeof(buf)];
     memset(zero, 0, sizeof(zero));
@@ -37,8 +42,18 @@ void zero(const std::string& path)
     size_t got = 0;
     while ((got = fread(buf, 1, sizeof(buf), fp)) > 0)
     {
-        fseek(fp, size, SEEK_SET);
-        fwrite(zero, 1, got, fp);
-        size = ftell(fp);
+        if (fseek(fp, size, SEEK_SET) != 0)
+            THROW(path << ": fseek failure");
+        if (ftell(fp) != size)
+            THROW(path << ": unexpected offset after fseek");
+        
+        size_t sent = fwrite(zero, 1, got, fp);
+        if (sent != got)
+            THROW(path << ": read " << got << " but only rewrote " << sent);
+        
+        size += sent;
+        
+        if (ftell(fp) != size)
+            THROW(path << ": unexpected offset after fwrite");
     }
 }
